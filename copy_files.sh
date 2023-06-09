0275cec8e967ac63850529b4b94d77e6fefe1f88
sfurlow8 <98120778+sfurlow8@users.noreply.github.com>
2023-06-08 20:51:16 -0500

    Update copy_files.sh

--- copy_files.sh
+++ copy_files.sh
@@ -1,1 +1,3 @@
 scp -i de300_group19.pem project_bonus.py ec2-user@ec2-13-58-91-51.us-east-2.compute.amazonaws.com:
+scp -i de300_group19.pem requirements.txt ec2-user@ec2-13-58-91-51.us-east-2.compute.amazonaws.com:
+scp -i de300_group19.pem pip_install.sh ec2-user@ec2-13-58-91-51.us-east-2.compute.amazonaws.com:
