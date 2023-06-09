9ef9c010a1cd48a2bfed72346bc6dd19bd3d3242
sfurlow8 <98120778+sfurlow8@users.noreply.github.com>
2023-06-08 20:47:19 -0500

    Add files via upload

---
+++ copy_files.sh
@@ -0,0 +1,1 @@
+scp -i de300_group19.pem project_bonus.py ec2-user@ec2-13-58-91-51.us-east-2.compute.amazonaws.com:
---
+++ login.sh
@@ -0,0 +1,1 @@
+ssh -i "de300_group19.pem" ec2-user@ec2-13-58-91-51.us-east-2.compute.amazonaws.com
